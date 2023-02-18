0ad55a0431d02a729be784ab02bbcef709960d28
DESKTOP-4TEIO0F\diego <diego.soto.lete@gmail.com>
2023-02-17 22:27:35 -0300

    test deploy github

--- server/server.js
+++ server/server.js
@@ -1,31 +1,67 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//FALTA EL LISTAR
-/* app.get('/api/products', async (req, res) => {
-  const products = await Product.find();
-  res.json(products);
-}); */
+try {
+  mongoose.connect('mongodb://localhost:27017/tu-base-de-datos', { useNewUrlParser: true, useUnifiedTopology: true });
+} catch (error) {
+  console.log(error);
+}
+// Definición del modelo de productos
 
+const productSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+  image: String
+});
+const Product = mongoose.model('Product', productSchema);
+
+// Middleware para manejar solicitudes HTTP incorrectas
+app.use((req, res, next) => {
+  if (['GET', 'POST', 'PUT', 'DELETE'].indexOf(req.method) !== -1) {
+    next();
+  } else {
+    res.status(405).send('Método no permitido');
+  }
+});
+
+
+// Ruta para obtener todos los productos
+
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error al obtener los productos');
+  }
+});
+
+
+// Ruta para crear un nuevo producto
 app.post('/api/products', async (req, res) => {
-    console.log('BBBB')
-  const product = {
-    name: req.body.name,
-    description: req.body.description,
-    price: req.body.price,
-    image: req.body.image,
-  };
-
-    //FALTA EL GUARDAR EN BD
-  /* await product.save(); */
-
-  res.json(product);
+  try {
+    const product = new Product({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      image: req.body.image
+    });
+    const objetoGuardado = await product.save();
+    res.send(objetoGuardado);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error al guardar el objeto');
+  }
 });
  
   
